9d51ee9aeb0331eaa6d05fd4234321b1cfaf9bfd
Zhonghao <zwang246@illinois.edu>
2019-09-24 22:28:29 -0500

    cls D

--- dataset/cityscapes_dataset.py
+++ dataset/cityscapes_dataset.py
@@ -6,8 +6,10 @@ import matplotlib.pyplot as plt
 import collections
 import torch
 import torchvision
+import torchvision.transforms as transforms
 from torch.utils import data
 from PIL import Image
+import pdb
 
 class cityscapesDataSet(data.Dataset):
     def __init__(self, root, list_path, max_iters=None, crop_size=(321, 321), mean=(128, 128, 128), scale=True, mirror=True, ignore_label=255, set='val'):
@@ -54,7 +56,148 @@ class cityscapesDataSet(data.Dataset):
         return image.copy(), np.array(size), name
 
 
+class cityscapesDataSetNorm(data.Dataset):
+    def __init__(self, root, list_path, max_iters=None, crop_size=(321, 321), mean=(128, 128, 128), scale=True, mirror=True, ignore_label=255, set='val'):
+        self.root = root
+        self.list_path = list_path
+        self.crop_size = crop_size
+        self.scale = scale
+        self.ignore_label = ignore_label
+        self.mean = mean
+        self.is_mirror = mirror
+        # self.mean_bgr = np.array([104.00698793, 116.66876762, 122.67891434])
+        self.img_ids = [i_id.strip() for i_id in open(list_path)]
+        if not max_iters==None:
+            self.img_ids = self.img_ids * int(np.ceil(float(max_iters) / len(self.img_ids)))
+        self.files = []
+        self.set = set
+        # for split in ["train", "trainval", "val"]:
+        for name in self.img_ids:
+            img_file = osp.join(self.root, "leftImg8bit/%s/%s" % (self.set, name))
+            self.files.append({
+                "img": img_file,
+                "name": name
+            })
+        self.normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        self.transform = transforms.Compose([
+                transforms.ToTensor(),
+                self.normalize,
+            ])
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, index):
+        datafiles = self.files[index]
+
+        image = Image.open(datafiles["img"]).convert('RGB')
+        name = datafiles["name"]
+        pdb.set_trace()
+        # resize
+        image = image.resize(self.crop_size, Image.BICUBIC)
+        image = self.transform(image)
+        image = np.asarray(image, np.float32)
+
+        size = image.shape
+        #image = image[:, :, ::-1]  # change to BGR
+        #image = image.transpose((2, 0, 1))
+
+        return image.copy(), np.array(size), name
+
+
+class cityscapesDataSetLabel(data.Dataset):
+    def __init__(self, root, list_path, max_iters=None, crop_size=(321, 321), mean=(128, 128, 128), scale=True, mirror=True, ignore_label=255, set='val'):
+        self.root = root
+        self.list_path = list_path
+        self.crop_size = crop_size
+        self.scale = scale
+        self.ignore_label = ignore_label
+        self.mean = mean
+        self.is_mirror = mirror
+        # self.mean_bgr = np.array([104.00698793, 116.66876762, 122.67891434])
+        self.img_ids = [i_id.strip() for i_id in open(list_path)]
+        if not max_iters==None:
+	        self.img_ids = self.img_ids * int(np.ceil(float(max_iters) / len(self.img_ids)))
+        self.files = []
+        self.set = set
+
+        self.id_to_trainid ={  0: 255,
+                              1: 255,
+                              2: 255,
+                              3: 255,
+                              4: 255,
+                              5: 255,
+                              6: 255,
+                              7:   0,
+                              8:   1,
+                              9: 255,
+                             10: 255,
+                             11:   2,
+                             12:   3,
+                             13:   4,
+                             14: 255,
+                             15: 255,
+                             16: 255,
+                             17:   5,
+                             18: 255,
+                             19:   6,
+                             20:   7,
+                             21:   8,
+                             22:   9,
+                             23:  10,
+                             24:  11,
+                             25:  12,
+                             26:  13,
+                             27:  14,
+                             28:  15,
+                             29: 255,
+                             30: 255,
+                             31:  16,
+                             32:  17,
+                             33:  18,
+                             -1: 255}
+
+        # for split in ["train", "trainval", "val"]:
+        for name in self.img_ids:
+            img_file = osp.join(self.root, "leftImg8bit/%s/%s" % (self.set, name))
+            label_file = osp.join(self.root, "gtFine/%s/%s" % (self.set, name[:-15]+'gtFine_labelIds.png'))
+            self.files.append({
+                "img": img_file,
+                "name": name,
+                "label": label_file
+            })
+
+    def __len__(self):
+        return len(self.files)
+
+    def __getitem__(self, index):
+        datafiles = self.files[index]
+
+        label = Image.open(datafiles["label"])
+        image = Image.open(datafiles["img"]).convert('RGB')
+        name = datafiles["name"]
+
+        # resize
+        label = label.resize(self.crop_size, Image.NEAREST)
+        label = np.asarray(label, np.uint8)
+        image = image.resize(self.crop_size, Image.BICUBIC)
+        image = np.asarray(image, np.float32)
+
+        # re-assign labels to match the format of Cityscapes
+        label_copy = 255 * np.ones(label.shape, dtype=np.uint8)
+        for k, v in self.id_to_trainid.items():
+            label_copy[label == k] = v
+
+        size = image.shape
+        image = image[:, :, ::-1]  # change to BGR
+        image -= self.mean
+        image = image.transpose((2, 0, 1))
+
+        return image.copy(), label_copy.copy(), np.array(size), name
+
+
 if __name__ == '__main__':
+
     dst = GTA5DataSet("./data", is_transform=True)
     trainloader = data.DataLoader(dst, batch_size=4)
     for i, data in enumerate(trainloader):
